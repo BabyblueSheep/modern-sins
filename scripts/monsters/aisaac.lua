dcbe23cc2dfc472b670cf48c69c7f10061fe6414
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2025-12-02 16:45:01 +0200

    working basic ai

--- scripts/monsters/aisaac.lua
+++ scripts/monsters/aisaac.lua
@@ -5,12 +5,23 @@ ModernSins.AIsaac.Variant = Isaac.GetEntityVariantByName("AIsaac")
 
 ModernSins.AIsaac.States = {}
 ModernSins.AIsaac.States.APPEAR = 0
-ModernSins.AIsaac.States.MOVING = 1
-ModernSins.AIsaac.States.SHOOT_TEARS = 2
+ModernSins.AIsaac.States.IDLE = 1
+ModernSins.AIsaac.States.MOVING = 2
+ModernSins.AIsaac.States.SHOOT_TEARS = 3
+
+local SWITCH_FROM_MOVING_TO_IDLE_CHANCE = 0.05
+local MOVING_MINIMUM_FRAME_AMOUNT = 30
+local SWITCH_FROM_IDLE_TO_MOVING_CHANCE = 0.1
+local IDLE_MINIMUM_FRAME_AMOUNT = 10
 
 local ATTACK_INITIAL_TIMER = 90
 local ATTACK_MINIMUM_TIMER = 30
 local ATTACK_MAXIMUM_TIMER = 90
+
+local ATTACK_TEAR_AMOUNT_MINIMUM = 3
+local ATTACK_TEAR_AMOUNT_MAXIMUM = 7
+local ATTACK_TEAR_SPEED = 5
+local ATTACK_TEAR_ANGLE_SPREAD = 45
 
 local ATTACK_TEARS_PARAMS = ProjectileParams()
 
@@ -21,6 +32,7 @@ ModernSins:AddCallback(ModCallbacks.MC_NPC_UPDATE, function (_, npc)
     end
 
     local data = npc:GetData()
+    local rng = npc:GetDropRNG()
 
     if not data.Init then
         npc:GetSprite():Play("Appear", true)
@@ -29,6 +41,7 @@ ModernSins:AddCallback(ModCallbacks.MC_NPC_UPDATE, function (_, npc)
         data.State = ModernSins.AIsaac.States.APPEAR
 
         data.AttackTimer = ATTACK_INITIAL_TIMER
+        data.StateFrameCount = 0
     end
 
     if data.State == ModernSins.AIsaac.States.APPEAR then
@@ -37,11 +50,25 @@ ModernSins:AddCallback(ModCallbacks.MC_NPC_UPDATE, function (_, npc)
             data.State = ModernSins.AIsaac.States.MOVING
         end
 
-    elseif data.State == ModernSins.AIsaac.States.MOVING then
+    elseif data.State == ModernSins.AIsaac.States.MOVING or data.State == ModernSins.AIsaac.States.IDLE then
         
-        local pathfinder = npc:GetPathfinder()
-        pathfinder:MoveRandomlyAxisAligned(0.9, false)
-        --npc:MultiplyFriction(0.8)
+        if data.State == ModernSins.AIsaac.States.MOVING then
+            local pathfinder = npc:GetPathfinder()
+            pathfinder:MoveRandomlyAxisAligned(0.9, false)
+
+            if data.StateFrameCount > MOVING_MINIMUM_FRAME_AMOUNT and rng:RandomFloat() < SWITCH_FROM_MOVING_TO_IDLE_CHANCE then
+                data.State = ModernSins.AIsaac.States.IDLE
+                data.StateFrameCount = 0
+            end
+        else
+            npc:MultiplyFriction(0.8)
+
+            if data.StateFrameCount > IDLE_MINIMUM_FRAME_AMOUNT and rng:RandomFloat() < SWITCH_FROM_IDLE_TO_MOVING_CHANCE then
+                data.State = ModernSins.AIsaac.States.MOVING
+                data.StateFrameCount = 0
+            end
+        end
+        data.StateFrameCount = data.StateFrameCount + 1
 
         if math.abs(npc.Velocity.X) > math.abs(npc.Velocity.Y) then
             npc:GetSprite():Play("WalkHori")
@@ -59,10 +86,27 @@ ModernSins:AddCallback(ModCallbacks.MC_NPC_UPDATE, function (_, npc)
         if data.AttackTimer <= 0 then
             
             data.State = ModernSins.AIsaac.States.SHOOT_TEARS
-            npc:GetSprite():Play("ShootDown")
+            npc:GetSprite():Play("ShootDown", true)
 
-            local tears = npc:FireProjectilesEx(npc.Position, )
+            SFXManager():Play(SoundEffect.SOUND_TEARS_FIRE)
 
+            local tearAmount = rng:RandomInt(ATTACK_TEAR_AMOUNT_MINIMUM, ATTACK_TEAR_AMOUNT_MAXIMUM)
+            local tearParamsVelocity = Vector(ATTACK_TEAR_SPEED, tearAmount)
+            local tears = npc:FireProjectilesEx(npc.Position, tearParamsVelocity, ProjectileMode.CIRCLE_CUSTOM, ATTACK_TEARS_PARAMS)
+            for i, tear in ipairs(tears) do
+                local angle = (rng:RandomFloat() * 2 - 1) * ATTACK_TEAR_ANGLE_SPREAD
+                tear.Velocity = tear.Velocity:Rotated(angle)
+            end
+
+        end
+
+    elseif data.State == ModernSins.AIsaac.States.SHOOT_TEARS then
+        
+        npc:MultiplyFriction(0.5)
+
+        if npc:GetSprite():IsFinished("ShootDown") then
+            data.State = ModernSins.AIsaac.States.MOVING
+            data.AttackTimer = rng:RandomInt(ATTACK_MINIMUM_TIMER, ATTACK_MAXIMUM_TIMER)
         end
 
     end
